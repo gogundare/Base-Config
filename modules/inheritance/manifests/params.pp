a5bf1e74b844aec7dca6f37b699527d8869db7f6
Gabriel Ogundare <gabbyogundare@gmail.com>
2016-10-11 11:37:15 -0400

    Update params.pp

--- modules/inheritance/manifests/params.pp
+++ modules/inheritance/manifests/params.pp
@@ -3,6 +3,7 @@ class inheritance::params {
         case $::osfamily {
                 'RedHat': { $ssh_name = 'sshd' }
                 'Debian': { $ssh_name = 'ssh' }
+                'Fedora': {$ssh_name = 'sshd'}
                 default: { fail('OS not supported by puppet modules SSH') }
         }
 }
